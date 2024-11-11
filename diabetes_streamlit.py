2360ddc7b1efecae31898eb0f547a23522940c2c
rafiilham91 <47430325+rafiilham91@users.noreply.github.com>
2024-11-11 11:58:11 +0700

    Update diabetes_streamlit.py
    menambah keterangan jk pd pop up text map

--- diabetes_streamlit.py
+++ diabetes_streamlit.py
@@ -86,11 +86,12 @@ elif page == "Dashboard Visualisasi":
             jk = row['jk']
             diagnosis = row['diagnosis']
 
+            # Ganti nilai jk menjadi "Laki-laki" atau "Perempuan"
+            jk = "Laki-laki" if row['jk'] == 0 else "Perempuan"
+            diagnosis = row['diagnosis']
+
             # Customize marker color based on diagnosis
-            if diagnosis == 1:
-                color = 'red'
-            else:
-                color = 'blue'
+            color = 'red' if diagnosis == 1 else 'blue'
 
             # Create a popup with information
             popup_text = f"<b>Kelurahan:</b> {kelurahan}<br><b>Umur:</b> {umur}<br><b>Jenis Kelamin:</b> {jk}<br><b>Diagnosis:</b> {diagnosis}"
